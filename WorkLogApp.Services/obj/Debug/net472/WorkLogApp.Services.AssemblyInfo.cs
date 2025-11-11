e0424bb1a5c3a2b97b8d808514a9b7059bdc59bb
abget <Abget-phi@outlook.com>
2025-11-11 16:59:25 +0800

    feat(模板管理): 在分类管理表单中添加快速插入占位符功能
    新增组合框和按钮用于快速选择并插入占位符到模板中
    支持双击表格行插入占位符，并为日期时间类型添加默认格式

--- WorkLogApp.Services/obj/Debug/net472/WorkLogApp.Services.AssemblyInfo.cs
+++ WorkLogApp.Services/obj/Debug/net472/WorkLogApp.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkLogApp.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36bbc2ccd586acec6a0ab74d5ac26f9d662e986d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31b5e99194a4958d2c19a00c5a65ccd64e80bbdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkLogApp.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkLogApp.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
