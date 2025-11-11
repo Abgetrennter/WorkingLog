1260b7db7efa9bad77e32519a47aa1c5094b487c
abget <Abget-phi@outlook.com>
2025-11-11 16:29:43 +0800

    feat(主界面): 添加工作日志列表视图和月份筛选功能
    在主界面添加列表视图显示工作日志条目，支持按月份筛选和导入当月Excel数据
    移除原有简单信息提示，改为功能完整的列表展示
    添加创建事项和导入按钮的事件处理

--- WorkLogApp.Services/obj/Debug/net472/WorkLogApp.Services.AssemblyInfo.cs
+++ WorkLogApp.Services/obj/Debug/net472/WorkLogApp.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkLogApp.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ab8cdf9b2b151b8236d450520006b8814aa30ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c6e74613ac257e7bd3a087e14cbab242b065735")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkLogApp.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkLogApp.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
