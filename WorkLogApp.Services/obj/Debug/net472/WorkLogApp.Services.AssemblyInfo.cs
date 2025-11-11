b9d05292b2551ce7dda206a6c70cc6970f0a1aee
abget <Abget-phi@outlook.com>
2025-11-11 17:29:14 +0800

    fix(ItemCreateForm): 移除重复的成功提示并直接关闭窗口
    编辑窗口已提示保存成功，因此移除创建界面中的重复提示并直接关闭窗口

--- WorkLogApp.Services/obj/Debug/net472/WorkLogApp.Services.AssemblyInfo.cs
+++ WorkLogApp.Services/obj/Debug/net472/WorkLogApp.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkLogApp.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd94fa1221f6fb7f2845b2def0c91304b4ea3042")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2643d859d1b3e1cff631d0d5a8d8ec9829728a91")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkLogApp.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkLogApp.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
