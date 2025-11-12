ee06d7902815231d6ccd466243d44477fbe2a2da
abget <Abget-phi@outlook.com>
2025-11-12 11:37:07 +0800

    feat(导入导出服务): 为Excel导出添加中文表头支持
    在导入导出服务中增加中文表头数组，当检测到现有表头为英文时自动替换为中文显示，提升中文用户的使用体验

--- WorkLogApp.Services/obj/Debug/net472/WorkLogApp.Services.AssemblyInfo.cs
+++ WorkLogApp.Services/obj/Debug/net472/WorkLogApp.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkLogApp.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+176dfe5d84a512ad46c58735c4a2774587f566fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e869ffcf4e36a28aef117f14bb1b04866f96a471")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkLogApp.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkLogApp.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
