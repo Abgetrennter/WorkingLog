6f0543ceafd7500e696ae5ab5c9e7ec9a2dd7dae
abget <Abget-phi@outlook.com>
2025-11-12 11:29:37 +0800

    refactor(UI): 移除主界面合并日志功能并调整按钮布局
    合并日志功能已整合至导入向导，移除冗余代码并更新相关文档

--- WorkLogApp.Services/obj/Debug/net472/WorkLogApp.Services.AssemblyInfo.cs
+++ WorkLogApp.Services/obj/Debug/net472/WorkLogApp.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkLogApp.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+addccc2f379642d4eef1956183efae1741f971ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+176dfe5d84a512ad46c58735c4a2774587f566fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkLogApp.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkLogApp.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
